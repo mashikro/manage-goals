a98eab2ccfa1c4881dffed740d280cf1f418f01c
Masha Ikromova <mashikro@gmail.com>
2019-12-15 12:59:24 -0800

    Wrote the connect_to_db func, update repr method for UserGoal class

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ class User(db.Model):
     fname = db.Column(db.String(50), nullable=False )
     lname = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), unique= True, nullable=False)
-    password_hash = db.Column(db.Text, nullable=False) #store password non securely
+    password = db.Column(db.Text, nullable=False) #store password non securely
 
 
     def __repr__(self):
@@ -38,6 +38,26 @@ class UserGoals(db.Model):
     def __repr__(self):
         """Provide helpful representation when printed."""
 
-        return '<User: email-{email} fname-{fname}>'.format(email=self.email, 
-                                                            fname=self.fname)
+        return '<user_email-{user_email} goal_text-{goal_text}>'.format(user_email=self.user.email, 
+                                                            goal_text=self.goal_text)
 
+################# DB SET UP #########################
+def connect_to_db(app, dbname):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PostgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql:///{dbname}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ECHO'] = True
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app 
+
+    connect_to_db(app, "goals")
+    print("Connected to DB.")
